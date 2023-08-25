a7d12efe026d863d25b6cfd812cec879da67fbf7
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-08-24 23:33:06 -0400

    feat: reorganize with company name first, then positions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/style.css"],
+  content: ["./src/style.css", "./src/resume.mdx"],
   theme: {
     extend: {},
   },
